42f9a5dc9142182c2f7d1d96baa57cc7453736b2
Kartikeya Bhatt <bhattkartikeya99@gmail.com>
2022-09-13 22:55:34 +0530

    Create  Brick Wall.java

---
+++ HASHMAP AND HEAP/Brick Wall.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int leastBricks(List<List<Integer>> wall) {
+      HashMap<Integer,Integer>map=new HashMap<>();
+        
+        for(int i=0;i<wall.size();i++)
+        {
+            int sum=0;
+            for(int j=0;j<wall.get(i).size()-1;j++)
+            {
+                sum+=wall.get(i).get(j);
+                map.put(sum,map.getOrDefault(sum,0)+1);
+            }
+        }
+        int max=0;
+        for(int val:map.values()){
+            max=Math.max(val,max);
+        }
+        
+        return (wall.size()-max);
+    }
+}
