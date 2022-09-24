d59a0aed6c08a932bbe4a2737ed0fd963ae1345d
Kartikeya Bhatt <bhattkartikeya99@gmail.com>
2022-09-24 08:28:16 +0530

    Update Daily Temperatures.java

--- STACK && QUEUEU/Daily Temperatures.java
+++ STACK && QUEUEU/Daily Temperatures.java
@@ -24,3 +24,27 @@ class Solution {
         return ans;
     }
 }
+
+// LEFT TO RIGHT 
+class Solution {
+    class Pair{
+        int idx,val;
+        Pair(int idx,int val){
+            this.idx=idx;
+            this.val=val;
+        }
+    }
+    public int[] dailyTemperatures(int[] temperatures) {
+        int []ans=new int[temperatures.length];
+        Stack<Pair>st=new Stack<>();
+        for(int i=0;i<temperatures.length;i++)
+        {
+            while(st.size()>0 && st.peek().val<temperatures[i]){
+                ans[st.peek().idx]=i-st.peek().idx;
+                st.pop();
+            }
+            st.push(new Pair(i,temperatures[i]));
+        }
+        return ans;
+    }
+}
