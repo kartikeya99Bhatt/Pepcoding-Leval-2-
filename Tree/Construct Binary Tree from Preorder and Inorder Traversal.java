ab46cf9cafab78cef77be3cc70f00374090c4f20
Kartikeya Bhatt <bhattkartikeya99@gmail.com>
2022-10-26 12:24:16 +0530

    Update Construct Binary Tree from Preorder and Inorder Traversal.java

--- Tree/Construct Binary Tree from Preorder and Inorder Traversal.java
+++ Tree/Construct Binary Tree from Preorder and Inorder Traversal.java
@@ -1,31 +1,29 @@
-class Solution {
-    public TreeNode buildTree(int[] preorder, int[] inorder) {
+class Solution
+{
+    public static Node buildTree(int inorder[], int preorder[], int n)
+    {
+        // code here 
         HashMap<Integer,Integer>map=new HashMap<>();
-        for(int i=0;i<inorder.length;i++)
-        {
+        for(int i=0;i<n;i++){
             map.put(inorder[i],i);
         }
-        TreeNode root=construct(0,preorder.length-1,0,inorder.length-1,preorder,map);
+        Node root=solve(0,inorder.length-1,0,preorder.length-1,preorder,map);
         return root;
     }
-    public TreeNode construct(int pre_lo,int pre_hi,int in_lo,int in_hi,int []preorder,HashMap<Integer,Integer>map)
-    {
-        // base case
-        if(pre_lo>pre_hi || in_lo>in_hi)
-        {
-          return null;    
-        }
-        
-        TreeNode node=new TreeNode();
-        
-        node.val=preorder[pre_lo];
-        int idx=map.get(node.val);
-        int diff=idx-in_lo;
-        
-        node.left=construct(pre_lo+1,pre_lo+diff  ,  in_lo,idx-1,preorder,map);
-        node.right=construct(pre_lo+diff+1,pre_hi  ,  idx+1,in_hi,preorder,map);
-        
-        return node;
-    }
     
+    public static Node solve(int i_lo,int i_hi,int p_lo,int p_hi,int []preorder,HashMap<Integer,Integer> map) 
+    {
+        if(i_hi<i_lo || p_hi<p_lo)
+        {
+            return null;
+        }
+        Node temp=new Node(0);
+        
+        temp.data=preorder[p_lo];
+        int idx=map.get(temp.data);
+        int diff=idx-i_lo;
+        temp.left=solve(i_lo,idx-1,p_lo+1,p_lo+diff,preorder,map);
+        temp.right=solve(idx+1,i_hi,p_lo+diff+1,p_hi,preorder,map);
+        return temp;
+    }
 }
