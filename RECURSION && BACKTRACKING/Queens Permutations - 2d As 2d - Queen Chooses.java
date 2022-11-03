5be91462b1bc911ef5dd2c93ba073e4600b5a41e
Kartikeya Bhatt <bhattkartikeya99@gmail.com>
2022-11-03 23:58:16 +0530

    Create Queens Permutations - 2d As 2d - Queen Chooses.java

---
+++ RECURSION && BACKTRACKING/Queens Permutations - 2d As 2d - Queen Chooses.java
@@ -0,0 +1,39 @@
+public static void queensPermutations(int qpsf, int tq, int[][] chess){
+        // write your code here
+        if(qpsf>tq)
+        {
+            for(int i=0;i<chess.length;i++)
+            {
+                for(int j=0;j<chess.length;j++)
+                {
+                    if(chess[i][j]!=0)
+                    System.out.print("q"+chess[i][j]+"	");
+                    else
+                    System.out.print("-	");
+                }
+                System.out.println();
+            }
+            System.out.println();
+            return ;
+        }
+        for(int i=0;i<chess.length;i++)
+        {
+            for(int j=0;j<chess.length;j++)
+            {
+                if(chess[i][j]==0)
+                {
+                    chess[i][j]=qpsf;
+                    queensPermutations(qpsf+1,tq,chess);
+                    chess[i][j]=0;
+                }
+            }
+        }
+        
+    }
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[][] chess = new int[n][n];
+        
+        queensPermutations(1, n, chess);
+    }
