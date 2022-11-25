cfd7054b19b0c83da38539423c2005f7aa4d0ad3
Kartikeya Bhatt <bhattkartikeya99@gmail.com>
2022-11-25 09:27:36 +0530

    Create Boats to Save People.java

---
+++ Arrays And String/Boats to Save People.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int numRescueBoats(int[] people, int limit) {
+        Arrays.sort(people);
+        int i=0;int j=people.length-1;int ans=0;
+        while(i<j)
+        {
+            if(people[i]+people[j]<=limit)
+            {
+                ans++;
+                i++;
+                j--;
+            }
+             else 
+              {
+                  ans++;
+                  j--;
+              }
+        }
+        
+        ans=(i==j)?ans+1:ans;
+        return ans;
+    }
+}
