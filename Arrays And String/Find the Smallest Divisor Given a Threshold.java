4707d2dacef8d5500f19cf1c2c17edc13e4a80f7
Kartikeya Bhatt <bhattkartikeya99@gmail.com>
2022-11-28 11:15:11 +0530

    Create  Find the Smallest Divisor Given a Threshold.java

---
+++ Arrays And String/Find the Smallest Divisor Given a Threshold.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int smallestDivisor(int[] nums, int threshold) {
+        int max=0;
+        for(int val:nums){
+            max=Math.max(val,max);
+        }
+        int hi=max; int lo=1;
+        while(lo<hi)
+        {
+            int mid=(lo+hi)/2;
+            int currSum=0;
+            for(int val:nums)
+            {
+                currSum+=Math.ceil(val*1.0/mid);
+            }
+            if(currSum<=threshold)
+            {
+                hi=mid;
+            }
+            else
+            {
+                lo=mid+1;
+            }
+        }
+        return lo;
+    }
+}
